f1d1b6275e071983b276762e714877e458207344
AmJoy01 <amber.lewis01@gmail.com>
2023-10-23 23:33:19 -0400

    redo event logic to work with react components

--- App.js
+++ App.js
@@ -1,24 +1,52 @@
 function App() {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const [board, setBoard] = React.useState(["", "", "", "", "", "", "", "", ""]);
+    let gameOver = false;
+    let turn = 'X';
+    let win;
+
+    function handleTurn(event) {
+        let idx = event.taget.id;
+        if (gameOver == false) {
+            let newBoard = [...board]
+            newBoard[idx] = turn
+            setBoard(newBoard)
+            turn = turn === 'X' ? 'O' : 'X';
+        }
+        // win = getWinner();
+        // if (win != null) {
+        //     boardId.removeEventListener('click', handleTurn)
+        //     addRestartMsg();
+        // }
+        // render();
+    };
+
+
+
+    // Return 
     return (
         <div>
             <h1>Tic-React-Toe</h1>
             <h1>Tic-Tac-Toe</h1>
             <h2>It's X's Turn</h2>
-            {/* <main>
-                <section id="score">Score for X: <span class="scoreX">0</span></section>
-                <section id="score">Score for O: <span class="scoreO">0</span></section>
-            </main> */}
-            <div class="flex-container flex-column">
-                <div class="flex-container flex-wrap" id="board">
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
+
+            <div className="flex-container flex-column">
+                <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
+                    {board.map((value, idx) => {
+                        return (
+                            <div className="square" key={idx} id={idx}>{value}</div>
+                        )
+                    })}
                 </div>
                 {/* End of board */}
                 <button id="reset-button">reset</button>
